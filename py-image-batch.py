326f3c9479bcbdb2d868565fba62ed7028f144b2
jmedrano87 <jmedrano87@users.noreply.github.com>
2022-01-26 20:20:02 -0800

    Resize function

--- py-image-batch.py
+++ py-image-batch.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from PIL import Image
 
 ArgParser = argparse.ArgumentParser(
     description="Resize all JPG files in source directory."
@@ -8,6 +9,15 @@ ArgParser.add_argument("-c", "--coefficient", type=float, default=0.5)
 ArgParser.add_argument("Source", type=Path)
 ArgParser.add_argument("Destination", type=Path)
 Args = ArgParser.parse_args()
+
+
+def ResizeImage(originalFile, newFile, multiplier):
+    img = Image.open(originalFile)
+    x = int(img.size[0] * multiplier)
+    y = int(img.size[1] * multiplier)
+    NewImg = img.resize((x, y), resample=Image.LANCZOS)
+    NewImg.save(newFile)
+
 
 print("Args passed", Args)
 
