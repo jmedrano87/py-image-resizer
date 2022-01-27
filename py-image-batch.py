87602612d1aeacad33c6b1f16326ec6f7466efec
jmedrano87 <jmedrano87@users.noreply.github.com>
2022-01-26 19:49:39 -0800

    Additional argument functionality

--- py-image-batch.py
+++ py-image-batch.py
@@ -5,13 +5,14 @@ import pathlib
 ArgParser = argparse.ArgumentParser(
     description="Resize all JPG files in source directory."
 )
+ArgParser.add_argument("-c", "--coefficient", type=float, default=0.5)
 ArgParser.add_argument("Source", type=pathlib.Path)
 ArgParser.add_argument("Destination", type=pathlib.Path)
 Args = ArgParser.parse_args()
 
 print("Args passed", Args)
 
-SourceDirectory = os.getcwd()
+SourceDirectory = Args.Source
 FileList = []
 
 for item in os.listdir(SourceDirectory):
@@ -22,5 +23,11 @@ if len(FileList) > 0:
     print("Found jpg files to process:")
     for File in FileList:
         print(File)
+    print(
+        "New files will be resized to",
+        Args.coefficient,
+        "and placed in:",
+        Args.Destination,
+    )
 else:
     print("Did not find any jpg files in " + SourceDirectory)
